255a4d7c445f32ee026b82c085df66490aac2552
Ramana2130 <ramanaraj7143@gmail.com>
2024-10-23 18:32:03 +0530

    database connected

--- src/main/config/db.config.ts
+++ src/main/config/db.config.ts
@@ -1,39 +1,46 @@
-import chalk from 'chalk'
+import chalk from 'chalk';
+import { Dialect } from 'sequelize';
+import { Sequelize } from 'sequelize-typescript';
 
-import { Dialect } from 'sequelize'
-import { Sequelize } from 'sequelize-typescript'
-
-// Models
-// import { User } from '../models/user';
-// import { BlacklistedToken } from '../models/BlacklistedToken';
-// import { cleanupExpiredTokens } from '../utils/tokens';
-
+// Import environment variables
 import {
   DATABASE_NAME,
   SQL_DIALECT,
   SQL_HOSTNAME,
   SQL_PASSWORD,
   SQL_USERNAME,
-} from '../constants/env'
+} from '../constants/env';
 
-const connection = new Sequelize({
+const sequelizeOptions = {
   dialect: SQL_DIALECT as Dialect,
-  host: SQL_HOSTNAME,
-  username: SQL_USERNAME,
-  password: SQL_PASSWORD,
-  database: DATABASE_NAME,
-  models: [],
+  models: [], // Add your models here
   logging: (sql): void => {
     console.log(
-      chalk.yellow('[ mssql ]'),
+      chalk.yellow('[ db ]'),
       sql,
       chalk.blue('at'),
       chalk.magenta(new Date().toLocaleTimeString()),
-    )
+    );
   },
-})
+};
 
-// clean up blacklist
-// setTimeout(cleanupExpiredTokens, ms('1h'));
+// Add connection options for dialects other than SQLite
+if (SQL_DIALECT !== 'sqlite') {
+  Object.assign(sequelizeOptions, {
+    host: SQL_HOSTNAME,
+    username: SQL_USERNAME,
+    password: SQL_PASSWORD,
+    database: DATABASE_NAME,
+  });
+} else {
+  // For SQLite, specify the storage path for the database
+  Object.assign(sequelizeOptions, {
+    storage: DATABASE_NAME,
+  });
+}
 
-export { connection }
+// Initialize Sequelize
+const connection = new Sequelize(sequelizeOptions);
+
+// Export the connection for use in other parts of your app
+export { connection };
